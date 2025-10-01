e0978f4a774a2155198651de1dc5103a3c0c7f0a
wearehereagain23 <wearehereagain23@gmail.com>
2025-09-30 18:27:40 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    FIREBASE: {
+        apiKey: "AIzaSyBKOGvOb_plKMaH_oec_E5wSY0shY27Z9E",
+        authDomain: "ge-lite.firebaseapp.com",
+        projectId: "ge-lite",
+        storageBucket: "ge-lite.appspot.com",
+        messagingSenderId: "1004534306152",
+        appId: "1:1004534306152:web:02b96781116ecdd70f72dd"
+    },
+    SUPABASE_URL: 'https://tkolyezukxoefqjzhqar.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrb2x5ZXp1a3hvZWZxanpocWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTU5MTMsImV4cCI6MjA2NzQzMTkxM30.S9flBLlO7p9IRA7XPZBMpc9lCC09PtveURimGeY0Nto',
+
+};
+
+
