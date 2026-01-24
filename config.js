28157f6b8b6d583f0bfe9ba5a8381a99ef6474d8
wearehereagain23 <wearehereagain23@gmail.com>
2026-01-24 09:04:46 -0800

    wew

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
+// config.js
 const CONFIG = {
-    FIREBASE: {
-        apiKey: "AIzaSyBKOGvOb_plKMaH_oec_E5wSY0shY27Z9E",
-        authDomain: "ge-lite.firebaseapp.com",
-        projectId: "ge-lite",
-        storageBucket: "ge-lite.appspot.com",
-        messagingSenderId: "1004534306152",
-        appId: "1:1004534306152:web:02b96781116ecdd70f72dd"
-    },
-    SUPABASE_URL: 'https://tkolyezukxoefqjzhqar.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrb2x5ZXp1a3hvZWZxanpocWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTU5MTMsImV4cCI6MjA2NzQzMTkxM30.S9flBLlO7p9IRA7XPZBMpc9lCC09PtveURimGeY0Nto',
-
+    SUPABASE_URL: 'https://xvwntavtpofolfeqtckn.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2d250YXZ0cG9mb2xmZXF0Y2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MjgyNzMsImV4cCI6MjA4NDQwNDI3M30.6kZ2tCp7wI7hBr_7PAYAQ6trBgZKs1beKeR6majGAts',
 };
 
-
+var supabaseDb;
+if (!supabaseDb) {
+    supabaseDb = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
+}
